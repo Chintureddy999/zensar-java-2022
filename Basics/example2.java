1cf2419b8bfa7f5e90e422d7055fac9bdda8fa5d
Chintureddy999 <abbavaramankr99@gmail.com>
2022-04-22 17:02:49 +0530

    push it

---
+++ Basics/example2.java
@@ -0,0 +1,17 @@
+package Basics;
+import java.util.Scanner;
+public class example2{
+    public static void CheckforAdult(int age) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println(a:"Enter a number");
+        int n = sc.nextInt();
+
+        if(a>=18){
+            System.out.println("chintu is an adult age is " + a);
+        }
+        else(a<18){
+            System.out.println("chintu is not an adult age is " + a);
+        }
+
+    }
+}
