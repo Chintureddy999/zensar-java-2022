1972625bf5e0e813b77afc2dc462591089834e20
Chintureddy999 <abbavaramankr99@gmail.com>
2022-04-22 17:04:06 +0530

    push example1

---
+++ example1.java
@@ -0,0 +1,14 @@
+public class example1{
+    public static void main(String[] args) {
+        String s = "chintu";
+        int x =22;
+
+        if(x>=18){
+            System.out.println("chintu is an adult age is " + x);
+        }
+        if(x<18){
+            System.out.println("chintu is not an adult age is " + x);
+        }
+
+    }
+}
