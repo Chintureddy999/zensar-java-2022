3cab2bdf076c102170c95f6ad5f13190519cd4fe
Chintureddy999 <abbavaramankr99@gmail.com>
2022-04-22 14:02:00 +0530

    push

---
+++ second.java
@@ -0,0 +1,12 @@
+public class second{
+    public static void main(String[] args) {
+        double a =55.9;
+        int b =9;
+
+        System.out.println("sum =" + (a+b));
+        System.out.println("substract=" + (a-b));
+        System.out.println("multiply =" + (a*b));
+        System.out.println("divide=" + (a/b));
+        
+    }
+}
