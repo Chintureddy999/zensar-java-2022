c6130e0a481550a2ce890b9a50e5349e327831eb
Chintureddy999 <abbavaramankr99@gmail.com>
2022-05-10 11:22:45 +0530

    new one aaryan

--- april/core_java/test.java
+++ april/core_java/test.java
@@ -8,10 +8,9 @@ public class test {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("size of String");
+    
         int n= sc.nextInt();
-        System.out.println();
-        System.out.println("String:");
+
 
         String arr[] = new String[n];
         for (int i = 0; i < n; i++)
