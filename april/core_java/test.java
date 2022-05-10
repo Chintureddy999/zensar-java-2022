c998935237c252d543aedb58e3b560cadf8a28d6
Chintureddy999 <abbavaramankr99@gmail.com>
2022-05-10 11:44:04 +0530

    please evaluate this aaryan

--- april/core_java/test.java
+++ april/core_java/test.java
@@ -4,25 +4,21 @@ import java.util.*;
 import java.util.Scanner;
 
 public class test {
-    private static String res;
+    
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("size of String");
-        int n= sc.nextInt();
-        System.out.println();
-        System.out.println("String:");
-
+        int n=sc.nextInt();
         String arr[] = new String[n];
-        for (int i = 0; i < n; i++)
 
+        for (int i = 0; i < n; i++)
         arr[i] = sc.next();
 
         int len = 0;
         ArrayList<String> oddLength = new ArrayList<String>();
-        for (int i = 0; i < n; i++) {
+        for (int i=0; i < n; i++) {
             len = arr[i].length();
-            if (len % 2 == 1)
+            if (len % 2 == 1);
             oddLength.add(arr[i]);
 
         }
@@ -30,10 +26,10 @@ public class test {
           System.out.println("Sorry Better luck next time");
         else
         {
-            Iterator<String> itr = oddLength.iterator();
-            int max = 1;
+            Iterator itr = oddLength.iterator();
+            int max =-1;
+            String res="";
             while (itr.hasNext())
-                ;
             {
                 String temp = (String) itr.next();
                 if (temp.length() > max) {
